7010dad25924fdce79204afe5caa527833733000
Jimmy Lee <jimmy@soomgo.com>
2021-11-14 14:02:37 +0900

    fix export

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,4 @@
-const config = {
+export = {
     env: {
         browser: true,
         es2021: true,
@@ -17,9 +17,9 @@ const config = {
     ],
     settings: {
         'import/parsers': {
-            '@typescript-eslint/parser': ['.ts', '.tsx'],
+            '@typescript-eslint/parser': [ '.ts', '.tsx' ],
         },
-        'import/extensions': ['.js', '.jsx', '.ts', '.tsx'],
+        'import/extensions': [ '.js', '.jsx', '.ts', '.tsx' ],
         'import/resolver': {
             node: {
                 extensions: [
@@ -51,8 +51,7 @@ const config = {
         // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-use-before-define.md#how-to-use
         // note you must disable the base rule as it can report incorrect errors
         'no-use-before-define': 'off',
-        '@typescript-eslint/no-use-before-define': ['error'],
+        '@typescript-eslint/no-use-before-define': [ 'error' ],
     },
 };
 
-export default config;
