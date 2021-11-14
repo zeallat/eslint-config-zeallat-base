486c18f4089a8470347af3f4bdcba79ab40df1ff
Jimmy Lee <jimmy@soomgo.com>
2021-11-14 13:25:00 +0900

    add index

---
+++ lib/index.ts
@@ -0,0 +1,56 @@
+export default {
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
+    },
+    extends: [
+        'airbnb-base',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 13,
+        sourceType: 'module',
+    },
+    plugins: [
+        '@typescript-eslint',
+    ],
+    settings: {
+        'import/parsers': {
+            '@typescript-eslint/parser': ['.ts', '.tsx'],
+        },
+        'import/extensions': ['.js', '.jsx', '.ts', '.tsx'],
+        'import/resolver': {
+            node: {
+                extensions: [
+                    '.js',
+                    '.jsx',
+                    '.ts',
+                    '.tsx',
+                    '.json',
+                    '.native.js',
+                    '.svg',
+                    '.gif',
+                    '.png',
+                ],
+            },
+        },
+    },
+    rules: {
+        'import/prefer-default-export': 'off',
+        'import/extensions': [
+            'error',
+            'ignorePackages',
+            {
+                js: 'never',
+                jsx: 'never',
+                ts: 'never',
+                tsx: 'never',
+            },
+        ],
+        // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-use-before-define.md#how-to-use
+        // note you must disable the base rule as it can report incorrect errors
+        'no-use-before-define': 'off',
+        '@typescript-eslint/no-use-before-define': ['error'],
+    },
+};
